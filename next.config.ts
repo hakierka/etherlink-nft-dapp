391f475e26b84a0d60c71d89f4e90bdbcd00017a
hakierka <amy.waliszewska@gmail.com>
2025-04-20 22:53:58 +0200

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-const nextConfig = {
-  reactStrictMode: true
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
 };
 
 export default nextConfig;
