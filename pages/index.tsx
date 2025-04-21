29fc17abb27dd49c036b13cec2e42d475f50c8a6
hakierka <amy.waliszewska@gmail.com>
2025-04-21 10:00:00 +0200

    update fetchnfts

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ declare global {
   }
 }
 
-const CONTRACT_ADDRESS = "0x349145BF727455Fd4fc8E547067960de07AB920e";
+const CONTRACT_ADDRESS = "0xa95a773c85eeb999e4ea2839434a7c7e232fd21c";
 const CONTRACT_ABI = [
   "function mint(address to, string memory tokenURI) public",
   "function transferNFT(address to, uint256 tokenId) public",
