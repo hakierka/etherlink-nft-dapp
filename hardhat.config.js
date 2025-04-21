6d0c80a4be7a3f35c40c26dd164157534e93a33d
hakierka <amy.waliszewska@gmail.com>
2025-04-21 09:20:11 +0200

    update hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     etherlink: {
       url: "https://node.ghostnet.etherlink.com",
-      accounts: ["7af750accc71f2d41c9ba65b3a50b75fa673df2ecf25214176f7fa60a9d02736"]
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
